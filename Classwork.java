6401c60b8e84437157b7cd1899c2c313691db5bc
tramngyn <bvnguyen115@student.hbuhsd.edu>
2024-09-27 13:52:24 -0700

    firstlastN finished

--- Classwork.java
+++ Classwork.java
@@ -2,9 +2,19 @@ public class Classwork
 {
   public static void main(String[] args)
   {
-    // test your code here
+    System.out.println(firstLastN("dictionary", 3));
   }
 
-  // write solutions to problems below
+  public static String firstLastN(String word, int n)
+  {
+    String output = new String();
+
+    String firstN = word.substring(0, n); // first n letters; substring from 0 to n-1
+    int indexOfNToLast = word.length() - n; // index of the nth to last letter
+    String lastN = word.substring(indexOfNToLast);
+
+    output = firstN + lastN;
+    return output;
+  }
 
 }
