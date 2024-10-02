94ecf67760259445ea7fc9138f0ec02486f15028
tramngyn <bvnguyen115@student.hbuhsd.edu>
2024-10-02 10:43:01 -0700

    finished

--- Classwork.java
+++ Classwork.java
@@ -3,6 +3,8 @@ public class Classwork
   public static void main(String[] args)
   {
     System.out.println(firstLastN("dictionary", 3));
+    System.out.println();
+    System.out.println(delEnd("library", 5));
   }
 
   public static String firstLastN(String word, int n)
@@ -17,4 +19,11 @@ public class Classwork
     return output;
   }
 
+  public static String delEnd(String word, int n)
+  {
+    int lastN = word.length() - n;
+    String end = word.substring(0, lastN);
+
+    return end;
+  }
 }
